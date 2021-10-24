a8fac2e3b7d392e42504c235fc4ef7543d0c3bfa
NoorMdKhan123 <noormd6400@gmail.com>
2021-10-24 19:05:00 +0530

    Done with prime numbers evaluation

---
+++ PrimeNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeNumbers
+{
+    class PrimeNumber
+    {
+        static bool isPrime(int n)
+        {
+            
+            if (n <= 1)
+                return false;
+
+            
+            for (int i = 2; i < n; i++)
+                if (n % i == 0)
+                    return false;
+
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        
+        bool isPrime = true;
+        int i, j;
+        
+        Console.WriteLine("Prime Numbers are : ");
+        for (i = 2; i <= 100; i++)
+        {
+            for (j = 2; j <= 100; j++)
+            {
+                if (i != j && i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+            {
+                Console.Write("\t" + i);
+            }
+            isPrime = true;
+        }
+     
+    }
+}
